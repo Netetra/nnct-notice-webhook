99771839235936c98b90ea4bf9209db7444674bc
Netetra <me@netetra.dev>
2023-05-13 03:36:48 +0900

    2023年  5月 13日 土曜日 03:36:48 JST

--- src/main.rs
+++ src/main.rs
@@ -6,24 +6,23 @@ use config::get_config;
 use discord::gen_msg;
 use nnct_notice::get_notices;
 use reqwest::{
-    header::{HeaderValue, CONTENT_TYPE},
     blocking::Client,
+    header::{HeaderValue, CONTENT_TYPE},
 };
 
 fn main() {
     const PAGE_URL: &str = "https://www.niihama-nct.ac.jp";
     let config = get_config("./config.toml").unwrap();
-
-    let notices = get_notices(PAGE_URL);
+    let notices = get_notices(PAGE_URL).unwrap();
     let messages = gen_msg(&config, notices);
     let client = Client::new();
     for message in messages {
         let json = match message.to_json() {
             Ok(json) => json,
-            Err(_) => continue
+            Err(_) => continue,
         };
-        println!("{}",json);
-        client
+        println!("{}", jsonxf::pretty_print(&json).unwrap());
+        let _ = client
             .post(&config.webhook_url)
             .header(CONTENT_TYPE, HeaderValue::from_static("application/json"))
             .body(json)
--- src/nnct_notice.rs
+++ src/nnct_notice.rs
@@ -14,15 +14,10 @@ pub struct Notice {
     pub url: String,
 }
 
-fn get_html(url: &str) -> Result<String, Box<dyn Error>> {
+fn get_root_element(url: &str) -> Result<Handle, Box<dyn Error>> {
     let client = Client::new();
     let res = client.get(url).send()?;
     let html = res.text()?;
-    return Ok(html);
-}
-
-fn get_root_element(url: &str) -> Result<Handle, Box<dyn Error>> {
-    let html = get_html(url)?;
     let soup = Soup::new(&html);
     let root_element = soup.get_handle();
     return Ok(root_element);
@@ -30,66 +25,62 @@ fn get_root_element(url: &str) -> Result<Handle, Box<dyn Error>> {
 
 fn get_element_from_class(parent_element: &Handle, class: &str) -> Option<Handle> {
     let element = parent_element.class(class).find()?.get_handle();
-    return Some(element)
+    return Some(element);
 }
 
 fn get_element_all_from_tag(parent_element: &Handle, tag: &str) -> Vec<Handle> {
-    return parent_element.tag(tag).find_all()
-        .map(|element| element.get_handle()).collect::<Vec<_>>();
-}
-
-// fn get_notice_section(root_element: Handle) -> Option<Handle> {
-//     let section = root_element
-//         .class("home-main-news-con")
-//         .find()?
-//         .get_handle();
-//     return Some(section);
-// }
-
-fn parse_anker(anker: Handle) -> Option<Anker> {
-    let content = anker.text();
-    let href = anker.get("href")?;
-    return Some(Anker { content, href });
-}
-
-// fn get_ankers(section: &Handle) -> Vec<Anker> {
-//     return section
-//         .tag("a")
-//         .find_all()
-//         .map(|anker| parse_anker(anker))
-//         .flatten()
-//         .collect::<Vec<_>>();
-// }
-
-fn join_content(content_element: &Handle) -> String {
-    return content_element
-        .tag("p")
+    return parent_element
+        .tag(tag)
         .find_all()
+        .map(|element| element.get_handle())
+        .collect::<Vec<_>>();
+}
+
+fn get_notice_section(parent_element: &Handle) -> Option<Handle> {
+    return get_element_from_class(parent_element, "home-main-news-con");
+}
+
+fn get_ankers(parent_element: &Handle) -> Vec<Handle> {
+    return get_element_all_from_tag(parent_element, "a");
+}
+
+fn parse_anker(anker_element: &Handle) -> Result<Anker, String> {
+    let content = anker_element.text();
+    let href = anker_element
+        .get("href")
+        .ok_or("anker href is void".to_string())?;
+    return Ok(Anker { content, href });
+}
+
+fn get_content(parent_element: &Handle) -> Option<String> {
+    let content_element = get_element_from_class(parent_element, "main-con")?;
+    let content_p_element = get_element_all_from_tag(&content_element, "p");
+    let content = join_content_p(&content_p_element);
+    return Some(content);
+}
+
+fn join_content_p(parent_element: &Vec<Handle>) -> String {
+    return parent_element
+        .into_iter()
         .map(|element| element.text())
         .collect::<Vec<String>>()
         .join("\n");
 }
 
-fn get_content(root_element: &Handle) -> Option<String> {
-    let content_element = root_element.class("main-con").find()?;
-    let content = join_content(&content_element);
-    return Some(content);
-}
-
 pub fn get_notices(url: &str) -> Result<Vec<Notice>, Box<dyn Error>> {
     let root_element = get_root_element(url)?;
-    let notice_section = get_element_from_class(&root_element, "home-main-news-con");
-    let ankers = get_element_all_from_tag(&notice_section, "a")
-        .into_iter().map(|anker| parse_anker(anker))
-        .flatten().collect::<Vec<_>>();
+    let notice_section_element =
+        get_notice_section(&root_element).expect("cannot get notice section.");
+    let ankers = get_ankers(&notice_section_element)
+        .into_iter()
+        .map(|anker| parse_anker(&anker))
+        .flatten()
+        .collect::<Vec<_>>();
     let notices = ankers
         .into_iter()
-        .map(|anker| {
+        .map(|anker| -> Option<Notice> {
             let content_url = url.to_string() + &anker.href;
-            let content_root_element = match get_root_element(&content_url) {
-                Ok(element) => element,
-                Err(_) => return None
-            };
+            let content_root_element = get_root_element(&content_url).ok()?;
             return Some(Notice {
                 title: anker.content,
                 description: get_content(&content_root_element),
